75c29df20e13d17d661dbf971489254f7114792e
BTozier <benjitenji@gmail.com>
2024-01-15 21:45:14 -0500

    change: reorganized and cleaned player widget

--- learn_qt_move/PlayerGridWidget.py
+++ learn_qt_move/PlayerGridWidget.py
@@ -4,47 +4,38 @@ from dataclasses import dataclass
 from functools import cache
 from typing import Optional
 
-from PyQt6.QtWidgets import QWidget
-from PyQt6.QtWidgets import QApplication
-from PyQt6.QtGui import QPainter
-from PyQt6.QtGui import QPen
-from PyQt6.QtGui import QBrush
-from PyQt6.QtGui import QPaintEvent
-from PyQt6.QtCore import Qt
-from PyQt6.QtCore import QRect
-from PyQt6.QtCore import QLine
-from PyQt6.QtCore import QPoint
-from PyQt6.QtCore import QSize
+from PyQt6.QtCore import QLine, QPoint, QRect, QSize, Qt
+from PyQt6.QtGui import QBrush, QPainter, QPaintEvent, QPen
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QVBoxLayout
 
 
-from common import PEN_BLACK_MEDIUM
-from common import BRUSH_BLUE_SOLID
+from common import BRUSH_BLUE_SOLID, PEN_BLACK_MEDIUM
 
 
-DEFAULT_GRID_SIZE_RECT = QRect(20, 20, 300, 300)
-DEFAULT_GRID_WIDGET_SIZE = QSize(340, 380)
-
+DEFAULT_GRID_AREA = QRect(20, 20, 300, 300)
+DEFAULT_TOTAL_SIZE = QSize(340, 380)
+DEFAULT_GRID_CELLS = QSize(5,5)
 
 class PlayerGridWidget(QWidget):
     """
     """
     
-    widget_size: QRect  # Size of the widget; grid + borders
-    grid_area: QRect    # Area where the grid is drawn
-    
+    # total_size: QSize  # Size of the widget; grid + borders
     num_cells: QSize    # Number of discrete spaces in the grid
     
-    def __init__(self, num_cells: QSize = QSize(5,5), widget_size: Optional[QSize] = None, grid_size: Optional[QSize] = None):
+    grid_area: QRect    # Size of area where the grid is drawn
+    
+    def __init__(
+        self,
+        num_cells: QSize = DEFAULT_GRID_CELLS,
+        grid_area: Optional[QRect] = DEFAULT_GRID_AREA
+        ):
         super().__init__()
         
-        try:
-            self.resize(*widget_size)
-            self.widget_size = widget_size
-        except(TypeError):
-            self.resize(DEFAULT_GRID_WIDGET_SIZE)
-            self.widget_size = DEFAULT_GRID_SIZE_RECT
+        self.setGeometry(grid_area)
+        self.grid_area = grid_area
         
-        self.grid_area = grid_size if grid_size and grid_size < self.widget_size else self.widget_size
         self.num_cells = num_cells 
         
         self.update()   # Trigger repaint
@@ -55,12 +46,15 @@ class PlayerGridWidget(QWidget):
         painter.setPen(QPen(*PEN_BLACK_MEDIUM))
         painter.setBrush(QBrush(*BRUSH_BLUE_SOLID))
         
+        # Set coordinates relative to grid area instead of the widget
+        painter.translate(self.grid_area.topLeft())     
+        
         # Draw Background
-        painter.drawRect(self.widget_size)
+        painter.drawRect(self.grid_area)
         
         # Draw Vertical lines for columns
         for h in range(1,self.num_cells.width()):
-            pos = self.grid_area.width() // self.num_cells.width() * h 
+            pos = self.grid_area.width() // self.num_cells.width() * h + self.grid_area.left()
             painter.drawLine(
                 QPoint(pos, self.grid_area.top()),      # (x,y)
                 QPoint(pos, self.grid_area.bottom())    # (dx,dy)
@@ -68,13 +62,15 @@ class PlayerGridWidget(QWidget):
             
         # Draw Horizontal lines for Rows
         for v in range(1,self.num_cells.height()):
-            pos = self.grid_area.height() // self.num_cells.height() * v
+            pos = self.grid_area.height() // self.num_cells.height() * v + self.grid_area.top()
             painter.drawLine(
                 QPoint(self.grid_area.left(), pos), 
                 QPoint(self.grid_area.right(), pos)
                 )
-        
+            
     
+def size_fits_in_rect(size: QSize, rect: QRect):
+    return size.width() <= rect.width() and size.height() <= rect.height()
             
 
 if __name__ == "__main__":
